bb8adab4f5eeb11a233db4bc478f7e0ea8729e86
Peter Rhodes <rhodespeter@hotmail.co.uk>
2017-01-27 13:04:35 +0000

    Add base files

---
+++ main.js
@@ -0,0 +1,26 @@
+window.onscroll = function() {
+  var nav = document.getElementsByClassName('fixed-header')[0];
+  if ( window.pageYOffset > 536 ) {
+      nav.classList.add("opacity");
+  } else {
+      nav.classList.remove("opacity");
+  }
+}
+
+elmnt = document.getElementById("projects");
+
+function scrollTo(element, to, duration) {
+  if (duration <= 0) return;
+  var difference = to - element.scrollTop;
+  var perTick = difference / duration * 10;
+
+  setTimeout(function() {
+    element.scrollTop = element.scrollTop + perTick;
+    if (element.scrollTop === to) return;
+    scrollTo(element, to, duration - 10);
+  }, 10);
+}
+
+document.getElementsByClassName("link-to-portfolio")[0].addEventListener("click", function(){
+  scrollTo(document.body, 537, 70);
+});
