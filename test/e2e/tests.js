97372a95d30b039397e9300b8e07d869ef7572e9
Peter Rhodes <rhodespeter@hotmail.co.uk>
2017-02-15 01:47:51 +0000

    remove tests file (keep test file)

--- test/e2e/tests.js
+++ test/e2e/tests.js
@@ -3,7 +3,7 @@ var config = require('../../nightwatch.conf.js');
 module.exports = {
   'Assert Elements': function(browser) {
     browser
-      .url('file:///Users/peterrhodes/Desktop/cv-portfolio/index.html')
+      .url(browser.launch_url)
       .waitForElementVisible('body')
       .saveScreenshot("tests/screenshots/screenshot.png")
       .assert.title('Peter Rhodes Portfolio')
